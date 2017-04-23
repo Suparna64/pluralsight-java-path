49dd3985c3c0b51d27ddfcfccf9580ccc0d58524
jc-sanchez <jc-sanchez@users.noreply.github.com>
2017-04-23 14:54:37 -0500

    Concurrency working for BankAccount example using 'synchronized

--- 02-the-core-platform/bank-example/src/com/jcsanchez/exercise/banking/Worker.java
+++ 02-the-core-platform/bank-example/src/com/jcsanchez/exercise/banking/Worker.java
@@ -13,11 +13,13 @@ public class Worker implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            int startBalance = account.getBalance();
-            account.deposit(10);
-            int endBalance = account.getBalance();
+            synchronized (account) {
+                int startBalance = account.getBalance();
+                account.deposit(10);
+                int endBalance = account.getBalance();
 
-            System.out.printf("Start: %-5d End: %-5d \n", startBalance, endBalance);
+                System.out.printf("Start: %-5d End: %-5d \n", startBalance, endBalance);
+            }
         }
     }
 }

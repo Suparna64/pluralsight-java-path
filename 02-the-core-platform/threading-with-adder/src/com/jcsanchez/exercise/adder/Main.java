59db583b08477c40f72656107ae4c2e7c97a7820
jc-sanchez <jc-sanchez@users.noreply.github.com>
2017-04-23 10:29:13 -0500

    Threading with threading pool using Adder

--- 02-the-core-platform/threading-with-adder/src/com/jcsanchez/exercise/adder/Main.java
+++ 02-the-core-platform/threading-with-adder/src/com/jcsanchez/exercise/adder/Main.java
@@ -1,23 +1,28 @@
 package com.jcsanchez.exercise.adder;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         String[] inFiles = {"./file1.txt", "./file2.txt", "./file3.txt", "./file4.txt", "./file5.txt", "./file6.txt"};
         String[] outFiles = {"./file1out.txt", "./file2out.txt", "./file3out.txt",
                              "./file4out.txt", "./file5out.txt", "./file6out.txt"};
-        Thread[] threads = new Thread[inFiles.length];
+        ExecutorService es = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < inFiles.length; i++) {
             Adder adder = new Adder(inFiles[i], outFiles[i]);
-            threads[i] = new Thread(adder);
-            threads[i].start();
+            es.submit(adder);
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        try {
+            es.shutdown();
+            es.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
     }
 }

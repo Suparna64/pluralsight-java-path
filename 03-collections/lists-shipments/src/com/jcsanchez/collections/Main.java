74c07011a7d4cb15bc0f3ce160499d8621b2e7d0
jc-sanchez <jc-sanchez@users.noreply.github.com>
2017-04-24 21:36:16 -0500

    Look at Lists using a shipments example.  Also ran JUnit tests

---
+++ 03-collections/lists-shipments/src/com/jcsanchez/collections/Main.java
@@ -0,0 +1,8 @@
+package com.jcsanchez.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ 03-collections/lists-shipments/src/com/jcsanchez/collections/Product.java
@@ -0,0 +1,32 @@
+package com.jcsanchez.collections;
+
+import java.util.Comparator;
+import static java.util.Comparator.comparing;
+
+/**
+ * Created by jsssn on 23-Apr-17.
+ */
+public class Product {
+    public static final Comparator<Product> BY_WEIGHT
+            = comparing(Product::getWeight);
+
+    private final String name;
+    private final int weight;
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() { return name; }
+
+    public int getWeight() {return weight; }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
